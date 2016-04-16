9b8b19a0942ad607b2e8f825fdbc14d42c6a72cd
MichaelProkofyev <mikeprokofyev@gmail.com>
2016-04-16 20:35:23 +0500

    pause menu gamepad support

--- Assets/NeonDrive/Game/GUI_NeonDrive/pause/pause.cs
+++ Assets/NeonDrive/Game/GUI_NeonDrive/pause/pause.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 
 public class pause : MonoBehaviour {
 
 
 
 	public GameObject canvasPause;
-
+	public Button returnButton, mainMenuButton;
 	public bool  yesPause;
-
+	private bool m_isAxisInUse = false;
 
 
 	public void Pause(){
@@ -38,90 +41,55 @@ public class pause : MonoBehaviour {
 
 
 	void Start()
-	
 	{
 		yesPause = false;
 		Time.timeScale = 1;
 		canvasPause.SetActive (false);
 		Cursor.visible = false;
+		returnButton = canvasPause.transform.FindChild ("return").GetComponent<Button>();
+		mainMenuButton = canvasPause.transform.FindChild ("mainmenu").GetComponent<Button>();
+		//Input.GetJoystickNames()
 	}
-
-
-
-
-
 
 	void Update()
-	       {
-
+	{
 		if (Input.GetButtonDown ("Cancel")) {
-			if(yesPause == true){
-				yesPause = false;
-
-			}
-
-			if(yesPause ==false){
-				yesPause = true;
+			yesPause = !yesPause;
 			
+			canvasPause.SetActive(yesPause);
+			Cursor.visible = yesPause;
+			Time.timeScale = yesPause ? 0 : 1;
+			
+		}
 
-
-			}
-
-			if(yesPause == true){
-				canvasPause.SetActive(true);
-				Time.timeScale = 0;
-				Cursor.visible = true;
-				
-
-			}
-			else
-			{
-				canvasPause.SetActive(false);
-				Time.timeScale = 1;
-				Cursor.visible = false;
-			}
-
-
-		
+		if (Input.GetButtonDown ("Submit")) {
+			//if(mainMenuButton.spriteState == SpriteState.)
+			EventSystem.current.currentSelectedGameObject.gameObject.GetComponent<Button>().onClick.Invoke();
 		}
 
 
 
-
-	}
-
-void OnApplicationPause()
-{
-	
-	
-		if (Input.GetButtonDown ("Cancel")) {
-			if(yesPause == true){
-			yesPause = false;
-			
-		}
-		
-		
-		
-		if(yesPause == true){   
-			canvasPause.SetActive(true);
-			Time.timeScale = 0;
-			Cursor.visible = true;
-			
-			
-		}
-		else
+		if( Input.GetAxisRaw("Vertical") != 0)
 		{
-			canvasPause.SetActive(false);
-			Time.timeScale = 1;
-			Cursor.visible = false;
-		}
-		
-		
+			if(m_isAxisInUse == false)
+			{
+				float v = Input.GetAxisRaw("Vertical");
+				Debug.Log (v);
+				if (0 < v) {
+					returnButton.Select();
+				}else if (v < 0){
+					mainMenuButton.Select();
+				}
+				m_isAxisInUse = true;
+			}
+		}else if( Input.GetAxisRaw("Vertical") == 0)
+		{
+			m_isAxisInUse = false;
+		}  
+
+
+
+
+
 	}
-
-
-
-	
-
-}
 }
